72c5b8fe911bed7413f6d5675803460a1a9196a7
aditiexcellence <aditi@excellencetechnologies.in>
2019-02-18 15:09:17 +0530

    assignment8

--- assignment8.js
+++ assignment8.js
@@ -4,24 +4,23 @@ $(document).ready(function () {
         url: 'assignment8.json',
         dataType: 'json',
         success: function (data) {
+            var record = data;
             var row = "";
-            for ($i = 0; $i < data.length; $i++) {
-                row += '<tr><td scope="row">' + data[$i].name + '</td>';
-                row += '<td>' + data[$i].email + '</td>';
-                row += '<td>' + data[$i].message + '</td>';
-                row += '<td>' + data[$i].date + '</td></tr>';
+            for ($i = 0; $i < 5; $i++) {
+                row += '<tr><td scope="row">' + record[$i].name + '</td>';
+                row += '<td>' + record[$i].email + '</td>';
+                row += '<td>' + record[$i].message + '</td>';
+                row += '<td>' + record[$i].date + '</td></tr>';
             }
             $('#tables').html(row);
             $('#page').after('<div id="nav"></div>');
             var rowsShown = 5;
-            var rowsTotal = $('#page tbody tr').length;
+            var rowsTotal = record.length;
             var numPages = rowsTotal / rowsShown;
             for (i = 0; i < numPages; i++) {
                 var pageNum = i + 1;
                 $('#nav').append('<a href="#" rel="' + i + '">' + pageNum + '</a> ');
             }
-            $('#page tbody tr').hide();
-            $('#page tbody tr').slice(0, rowsShown).show();
             $('#nav a:first').addClass('active');
             $('#nav a').bind('click', function () {
 
@@ -30,11 +29,17 @@ $(document).ready(function () {
                 var currPage = $(this).attr('rel');
                 var startItem = currPage * rowsShown;
                 var endItem = startItem + rowsShown;
-                $('#page tbody tr').css('opacity', '0.0').hide().slice(startItem, endItem).
-                    css('display', 'table-row').animate({ opacity: 1 }, 300);
+                var row = "";
+                for ($i = startItem; $i < endItem; $i++) {
+                    row += '<tr><td scope="row">' + record[$i].name + '</td>';
+                    row += '<td>' + record[$i].email + '</td>';
+                    row += '<td>' + record[$i].message + '</td>';
+                    row += '<td>' + record[$i].date + '</td></tr>';
+                }
+                $('#tables').html(row);
             });
         }
-    });
+    })
     location: 'assignment8.html';
     setTimeout(function () {
         location.reload();
