f0df8cc6b89fc0d8b1cd35a1203fb3b905b4e331
aditiexcellence <aditi@excellencetechnologies.in>
2019-02-19 10:33:56 +0530

    assignment8

--- assignment8.js
+++ assignment8.js
@@ -1,71 +1,59 @@
+var jsonobject;
 $(document).ready(function () {
-    var jsonobject;
     $.ajax({
         type: 'GET',
         url: 'assignment8.json',
         dataType: 'json',
         success: function (data) {
             jsonobject = data;
+            pagination();
         }
     })
     var currentdata;
-    $.ajax({
-        // type: 'GET',
-        // url: 'assignment8.json',
-        // dataType: 'json',
-        success: function () {
-            var record = jsonobject;
-            // var row = "";
-            // for ($i = 0; $i < 5; $i++) {
-            //     row += '<tr><td scope="row">' + record[$i].name + '</td>';
-            //     row += '<td>' + record[$i].email + '</td>';
-            //     row += '<td>' + record[$i].message + '</td>';
-            //     row += '<td>' + record[$i].date + '</td></tr>';
-            // }
-            // $('#tables').html(row);
-            $('#page').after('<div id="nav"></div>');
-            var rowsShown = 5;
-            var rowsTotal = record.length;
-            var numPages = rowsTotal / rowsShown;
-            for (i = 0; i < numPages; i++) {
-                var pageNum = i + 1;
-                $('#nav').append('<a href="#" rel="' + i + '">' + pageNum + '</a> ');
-            }
-            $('#nav a:first').addClass('active');
-            $('#nav a').bind('click', function () {
-                $('#nav a').removeClass('active');
-                $(this).addClass('active');
-                var currPage = $(this).attr('rel');
-                var startItem = currPage * rowsShown;
-                var endItem = startItem + rowsShown;
-                var row = "";
-                for ($i = startItem; $i < endItem; $i++) {
-                    currentdata = record;
-                    row += '<tr class="myhead"><td scope="row">' + record[$i].name + '</td>';
-                    row += '<td>' + record[$i].email + '</td>';
-                    row += '<td>' + record[$i].message + '</td>';
-                    row += '<td>' + record[$i].date + '</td></tr>';
-                }
-                $('#tables').html(row);
-            });
+    function pagination() {
+        $('#page').after('<div id="nav"></div>');
+        var rowsShown = 5;
+        var rowsTotal = jsonobject.length;
+        var numPages = rowsTotal / rowsShown;
+        for (i = 0; i < numPages; i++) {
+            var pageNum = i + 1;
+            $('#nav').append('<a href="#" rel="' + i + '">' + pageNum + '</a> ');
         }
-    })
-    $('#search_field').on('keyup', function () {
-        var value = $(this).val();
-        var patt = new RegExp(value, "i");
-        $('#myTable').find('tr').each(function () {
-            if (!($(this).find('td').text().search(patt) >= 0)) {
-                $(this).not('.myHead').hide();
-            }
-            if (($(this).find('td').text().search(patt) >= 0)) {
-                $(this).show();
-            }
+        $('#nav a:first').addClass('active');
+        $('#nav a').bind('click', function () {
+            showPage($(this).attr("rel"));
         });
+        function showPage(currPage) {
+            $('#nav a').removeClass('active');
+            $('#nav a:eq(' + currPage + ')').addClass('active');
+            var startItem = currPage * rowsShown;
+            var endItem = startItem + rowsShown;
+            var row = "";
+            for ($i = startItem; $i < endItem; $i++) {
+                currentdata = jsonobject[$i];
+                row += '<tr class="myhead"><td scope="row">' + currentdata.name + '</td>';
+                row += '<td>' + currentdata.email + '</td>';
+                row += '<td>' + currentdata.message + '</td>';
+                row += '<td>' + currentdata.date + '</td></tr>';
+            }
+            $('#tables').html(row);
+        }
+        showPage(0);
+    }
+    $('#search').keyup(function () {
+        var search = $(this).val();
+        $('table tbody tr').hide();
+        var len = $('table tbody tr:not(.notfound) td:nth-child(2):contains("' + search + '")').length;
+        if (len > 0) {
+            $('table tbody tr:not(.notfound) td:contains("' + search + '")').each(function () {
+                $(this).closest('tr').show();
+            });
+        } else {
+            $('.notfound').show();
+        }
     });
-
     location: 'assignment8.html';
     setTimeout(function () {
         location.reload();
     }, 5000);
-
 })
